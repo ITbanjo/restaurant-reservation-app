1aee6fdac43ab777e1763dfb3c328f654f61a5ad
Isaac Taylor <banjertaylor@gmail.com>
2024-04-10 18:02:39 -0400

    Added functionality so that /dashboard?date='current date' appears in URL from dashboard menu link, and upon inital /dashboard screen render

--- front-end/src/layout/Menu.js
+++ front-end/src/layout/Menu.js
@@ -2,6 +2,8 @@ import React from "react";
 
 import { Link } from "react-router-dom";
 
+import { today } from "../utils/date-time";
+
 /**
  * Defines the menu for this application.
  *
@@ -23,7 +25,7 @@ function Menu() {
         <hr className="sidebar-divider my-0" />
         <ul className="nav navbar-nav text-light" id="accordionSidebar">
           <li className="nav-item">
-            <Link className="nav-link" to="/dashboard">
+            <Link className="nav-link" to={`/dashboard?date=${today()}`}>
               <span className="oi oi-dashboard" />
               &nbsp;Dashboard
             </Link>
