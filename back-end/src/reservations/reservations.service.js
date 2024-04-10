c385d6f824caaeeb2804aa1955fc7235f9f61d74
Isaac Taylor <banjertaylor@gmail.com>
2024-04-10 16:34:05 -0400

    Removed knex.raw() from getReservationsForSpecifiedDate query builder

--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -3,9 +3,12 @@ const knex = require("../db/connection");
 function getReservationsForSpecifiedDate(date) {
   return knex("reservations")
     .distinct(
-      knex.raw(
-        "first_name, last_name, mobile_number, reservation_date, reservation_time, people"
-      )
+      "first_name",
+      "last_name",
+      "mobile_number",
+      "reservation_date",
+      "reservation_time",
+      "people"
     )
     .where({ reservation_date: date })
     .orderBy("reservation_time");
